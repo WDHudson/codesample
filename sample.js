dd54defeb87ddb3a8483bb8711126a7a52d79f0d
WDHudson <43639901+WDHudson@users.noreply.github.com>
2019-06-26 09:53:07 -0500

    Update sample.js

--- sample.js
+++ sample.js
@@ -3,3 +3,5 @@ console.log('this is a sameple to see if I\'m doing it right on git')
 // this is the second line of code to see what the terminal says
 
 // let\'s try this again
+
+// this is to test if I can pull a file from the master and see the new changes made
