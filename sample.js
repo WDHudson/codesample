e5f2f94ed1ac26550b9d6885c58f7c37b115ea6d
William Hudson <your_email@whatever.comwilliamdhudson@gmail.com>
2019-06-27 08:40:37 -0500

    Another example of pushing edited files back up to GitHub

--- sample.js
+++ sample.js
@@ -3,3 +3,5 @@ console.log('this is a sameple to see if I\'m doing it right on git')
 // this is the second line of code to see what the terminal says
 
 // this is a test to see if what I pull from GitHub shows up in my file
+
+// Now this is another test if those directions work for pushing back onto GitHub
